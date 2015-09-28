a715eaf1f466174d351266b31e8022eae211930c
Ben Burry <bburry@etsy.com>
2015-09-29 00:54:30 +0100

    Add cors header to allow plugin access

--- tometoyou.py
+++ tometoyou.py
@@ -67,6 +67,7 @@ def pics(timezone):
     response.headers['Cache-Control'] = 'no-store, no-cache, must-revalidate, post-check=0, pre-check=0, max-age=0'
     response.headers['Pragma'] = 'no-cache'
     response.headers['Expires'] = '-1'
+    response.headers['Access-Control-Allow-Origin'] = '*'
     return response
 
 
