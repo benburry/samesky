5d7811094dead96f0bb2ade69c97e64cdaa617de
Ben Burry <bburry@etsy.com>
2015-10-21 12:43:59 +0100

    Allow switching between the two timezones in newtab

--- chrome_extension/script/samesky.js
+++ chrome_extension/script/samesky.js
@@ -1,4 +1,8 @@
 $(document).ready(function () {
+  var menotyou = true;
+  var datacache;
+
+
   chrome.storage.sync.get({
     endpoint_url: 'http://home.burry.name:5000'
   }, function(items) {
@@ -9,23 +13,37 @@ $(document).ready(function () {
     if (typeof (tz) !== 'undefined') {
         tz_name = tz.name();
 
-        var update = function() {
-            $.getJSON(baseurl + '/getpics/' + tz_name, function(data) {
-                if (data.me.img) {
+        var render = function() {
+            if (datacache) {
+                var whichdata = menotyou ? datacache.me : datacache.you;
+                if (whichdata.img) {
                     img = new Image();
                     img.onload = function () {
-                        $('#tz').html(data.me.display_tz);
-                        $('#time').html(data.me.time);
+                        $('#tz').html(whichdata.display_tz);
+                        $('#time').html(whichdata.time);
 
                         $('html').css('background', "url('" + img.src + "') no-repeat center center fixed");
                         $('html').css('-webkit-background-size', 'cover');
                     };
-                    img.src = baseurl + data.me.img;
+                    img.src = baseurl + whichdata.img;
                 }
+            }
+        };
+
+        var update = function() {
+            $.getJSON(baseurl + '/getpics/' + tz_name, function(data) {
+                datacache = data;
+                render();
             });
         };
+
+        $("#time").click(function() {
+            menotyou = !menotyou;
+            render();
+        });
     }
     update();
     setInterval(update, 60000);
   });
+
 });
